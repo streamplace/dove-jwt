b258c62e185d4e7717c950418974cb4daafec35c
Eli Mallon <eli@stream.kitchen>
2017-01-03 04:35:33 +0000

    Add domain verification

--- src/dove-jwt.js
+++ src/dove-jwt.js
@@ -1,9 +1,13 @@
 
 // Why is this KJUR?
 var jsrsasign = require("jsrsasign");
+var forge = require("node-forge");
+var x509 = require("x509");
 var fs = require("fs");
 var path = require("path");
-var forge = require("node-forge");
+var debug = require("debug");
+
+const log = debug("dove-jwt");
 
 var ASN1HEX = jsrsasign.ASN1HEX;
 var JWS = jsrsasign.jws.JWS;
@@ -32,6 +36,23 @@ function splitca(chain, split) {
   }
   return ca;
 }
+
+var caStore = forge.pki.createCaStore();
+const myCAs = fs.readFileSync("/etc/ssl/certs/ca-certificates.crt", "utf8");
+
+let added = 0;
+let failed = 0;
+splitca(myCAs).forEach(ca => {
+  try {
+    caStore.addCertificate(ca)
+    added += 1;
+  }
+  catch (e) {
+    // This is fine -- forge only supports RSA, so lots of these roots won't work.
+    failed += 1;
+  }
+});
+log(`${added} CAs added, ${failed} CAs failed. (This is usually because node-forge only supports RSA.)`);
 
 function pemToDerArray(pem) {
   return splitca(pem).map((pem) => {
@@ -71,8 +92,16 @@ const certChain = derArrayToPem(headerObj.x5c);
 var pubKey = KEYUTIL.getKey(certChain);
 var isValid = JWS.verify(myJws, pubKey, ["RS256"]);
 
+log(isValid ? "valid!" : "invalid!");
 
-console.log(isValid ? "valid!" : "invalid!");
-console.log(payloadObj);
+try {
+  log("before");
+  const result = forge.pki.verifyCertificateChain(caStore, splitca(certChain).map(forge.pki.certificateFromPem));
+}
+catch (e) {
+  console.error(e.message);
+  throw new Error(e.message);
+}
+log("after");
 // const myJws = KJUR.jws.JWS.sign("RS256", header, {foo: "bar"}, key);
 
