f5121f9e59310606748f3ca2e0ee4c37d44f118d
Eli Mallon <eli@stream.kitchen>
2017-01-04 22:31:42 +0000

    Sign fake CA cert

--- __test__/certs/openssl.sh
+++ __test__/certs/openssl.sh
@@ -8,7 +8,7 @@ docker run \
   --rm \
   -it \
   -v $(realpath .):/certs \
-  -e OPENSSL_CONF=/certs/Fake_Root_Certificate_Authority.cnf \
+  -e OPENSSL_CONF=/certs/openssl/openssl.cnf \
   -w /certs \
   centurylink/openssl \
   sh -c "openssl $*"
