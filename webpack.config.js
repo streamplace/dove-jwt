6cb7723499f5e124f4528069371967e146ddbc69
Eli Mallon <eli@stream.place>
2017-08-06 19:04:30 -0700

    Working, if primitive, browser implementation

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ module.exports = {
   context: __dirname + "/src",
   entry: "./dove-jwt",
   output: {
+    library: "DoveJWT",
+    libraryTarget: "umd",
     path: __dirname + "/docs",
     filename: "dove-jwt.webpack.js"
   },
